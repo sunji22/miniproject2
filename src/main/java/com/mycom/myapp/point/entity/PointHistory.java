df6964ff9af0e51bbc3d3b925009360413bf414f
kyd4947 <kyd4947@gmail.com>
2026-07-16 15:55:02 +0900

    feat: 1-PointHistory-엔티티-작성

---
+++ src/main/java/com/mycom/myapp/point/entity/PointHistory.java
@@ -0,0 +1,47 @@
+package com.mycom.myapp.point.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="point_history")
+public class PointHistory {
+
+	// 기본 키 설정 (BIGINT -> Long 타입으로 매핑)
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="point_history_id")
+	private Long pointHistoryid;
+	
+	@ManyToOne(fetch=FetchType.LAZY)		// 포인트 이력 조회 많을 수 있기 때문에 지연 로딩 처리
+	@JoinColumn(name="user_id", nullable=false)
+	private User user;
+	
+	@ManyToOne(fetch=FetchType.LAZY)		// 포인트 이력 조회 많을 수 있기 때문에 지연 로딩 처리
+	@JoinColumn(name="participation_id")	// 정산 or 충전은 참여 내역에 없을 수도 있어서 nullable=true
+	private Participation participation;
+	
+	@Column(nullable=false)
+	private int amout;
+	
+	@Enumerated(EnumType.STRING)			// DB -> "CHARGE/DEPOSIT_LOCK/DEPOSIT_REFUND/PENALTY/REWARD" 문자열 저장
+	@Column(nullable=false)
+	private PoinType type;
+	
+	@Column(name="balance_after", nullable=false)
+	private int balaceAfter;
+	
+	@Column(name="created_at", nullable=false, updatable=false)
+	private LocalDateTime createdAt = LocalDateTime.now();
+}
