288f2129d03e6432a72b9d65d3014a6414c40bd8
kyd4947 <kyd4947@gmail.com>
2026-07-16 16:01:40 +0900

    feat:2-보증금-상태-목록-작성

---
+++ src/main/java/com/mycom/myapp/point/entity/PointType.java
@@ -0,0 +1,9 @@
+package com.mycom.myapp.point.entity;
+
+public enum PointType {
+	CHARGE,					// 포인트 충전
+	DEPOSIT_LOCK,			// 챌린지 참여 -> 보증금 잠금
+	DEPOSIT_REFUND,			// 챌린지 성공 -> 보증금 환불
+	PENALTY,				// 챌린지 실패 -> 보증금 몰수
+	REWARD,					// 몰수분 분배
+}
