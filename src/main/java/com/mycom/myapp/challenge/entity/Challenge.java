5ba41441a57e6e9ac6941af24eba9602b8426455
이승지 <tmdwl7978@naver.com>
2026-07-20 10:58:02 +0900

    refactor: 챌린지 엔티티 id 컬럼명 명시

--- src/main/java/com/mycom/myapp/challenge/entity/Challenge.java
+++ src/main/java/com/mycom/myapp/challenge/entity/Challenge.java
@@ -3,6 +3,7 @@ package com.mycom.myapp.challenge.entity;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +23,7 @@ public class Challenge {
 
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "challenge_id")
 	private Long id;
 	
 //	@ManyToOne(fetch = FetchType.LAZY)
