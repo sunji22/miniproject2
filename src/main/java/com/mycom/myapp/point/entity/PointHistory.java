bd9fbc5580f8b1249c8770b0a9eac390d0f73336
kyd4947 <kyd4947@gmail.com>
2026-07-20 15:15:51 +0900

    refactor: 12-PointHistory-import수정

--- src/main/java/com/mycom/myapp/point/entity/PointHistory.java
+++ src/main/java/com/mycom/myapp/point/entity/PointHistory.java
@@ -2,6 +2,9 @@ package com.mycom.myapp.point.entity;
 
 import java.time.LocalDateTime;
 
+import com.mycom.myapp.challenge.entity.Participation;
+import com.mycom.myapp.user.entity.User;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
