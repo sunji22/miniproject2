fb878b72e5d7d0268d9388739398c75ea24f0db0
kyd4947 <kyd4947@gmail.com>
2026-07-22 13:50:35 +0900

    feat: 4-PointHistoryRepository-정산금액조회-구현

--- src/main/java/com/mycom/myapp/point/repository/PointHistoryRepository.java
+++ src/main/java/com/mycom/myapp/point/repository/PointHistoryRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.mycom.myapp.point.entity.PointHistory;
+import com.mycom.myapp.point.entity.PointType;
 
 // 쿼리 자동 생성을 위해 JpaRepository 상속
 @Repository
@@ -13,4 +14,7 @@ public interface PointHistoryRepository extends JpaRepository<PointHistory, Long
 	
 	// 특정 회원(회원 id 를 통해) 포인트 이력 조회 (최신순)
 	List<PointHistory> findByUser_UserIdOrderByCreatedAtDesc(Long userId);
+	
+	// 챌린지별 정산 금액 합계 조회
+	Integer sumAmountByParticipation_Challenge_IdAndTypeIn(Long challengeId, List<PointType> types);
 }
