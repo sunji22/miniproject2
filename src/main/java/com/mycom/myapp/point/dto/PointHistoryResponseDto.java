0f3cc9db601857342fd8b394b9b590ea181ff50c
kyd4947 <kyd4947@gmail.com>
2026-07-22 09:28:43 +0900

    feat: 5-PointHistoryResponseDto-이력조회-필드추가

--- src/main/java/com/mycom/myapp/point/dto/PointHistoryResponseDto.java
+++ src/main/java/com/mycom/myapp/point/dto/PointHistoryResponseDto.java
@@ -1,5 +1,10 @@
 package com.mycom.myapp.point.dto;
 
+import java.time.LocalDateTime;
+
+import com.mycom.myapp.point.entity.PointHistory;
+import com.mycom.myapp.point.entity.PointType;
+
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,4 +12,19 @@ import lombok.Data;
 @Builder
 public class PointHistoryResponseDto {
 	private int balance;
+	private Long pointHistoryId;
+	private int amount;
+	private PointType type;
+	private int balanceAfter;
+	private LocalDateTime createdAt;
+	
+	public static PointHistoryResponseDto from(PointHistory entity) {
+		return PointHistoryResponseDto.builder()
+				.pointHistoryId(entity.getPointHistoryId())
+				.amount(entity.getAmount())
+				.type(entity.getType())
+				.balanceAfter(entity.getBalanceAfter())
+				.createdAt(entity.getCreatedAt())
+				.build();
+	}
 }
