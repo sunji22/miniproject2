70f126ae60763c0d5e2b9173313df1b22cbbbd79
kyd4947 <kyd4947@gmail.com>
2026-07-22 16:13:42 +0900

    feat: 11-ChallengeApplication-스케줄링 어노테이션 추가

--- src/main/java/com/mycom/myapp/ChallengeApplication.java
+++ src/main/java/com/mycom/myapp/ChallengeApplication.java
@@ -2,8 +2,10 @@ package com.mycom.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling			// 정산 자동 처리를 위한 스케줄링 어노테이션 추가
 public class ChallengeApplication {
 
     public static void main(String[] args) {
